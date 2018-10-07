2a3d2bf6b04f0fd6eeafa54330821ce657c93e41
Mykola <mykola.lisivka@gmail.com>
2018-10-07 20:03:58 +0300

    Added more specs

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe OrdersController, type: :controller do
   describe '#create' do
-    context 'when the data are valid' do
+    context 'when the attributes are valid' do
       before do
         post :create, params: { order: attributes_for(:order) }
       end
@@ -14,20 +14,60 @@ RSpec.describe OrdersController, type: :controller do
       it 'render notice created' do
         expect(json['notice']).to eq 'Created'
       end
+
+      it 'save the new order in database' do
+        expect do
+          post :create, params: { order: attributes_for(:order) }
+        end.to change(Order, :count).by(1)
+      end
     end
 
-    context 'when the data are invalid' do
-      before do
-        post :create, params: { order: attributes_for(:order, amount: nil) }
+    context 'when the attributes are invalid' do
+      context 'with invalid amount' do
+        it 'render errors' do
+          post :create, params: { order: attributes_for(:order, amount: nil) }
+
+          expect(json['errors']['amount'])
+            .to match_array(["can't be blank", 'is not a number'])
+        end
+
+        it_behaves_like 'invalid order attributes', :amount
       end
 
-      it 'render http bad request' do
-        expect(response).to have_http_status(:bad_request)
+      context 'with invalid first_name' do
+        it 'render errors' do
+          post :create,
+            params: { order: attributes_for(:order, first_name: nil) }
+
+          expect(json['errors']['first_name'])
+            .to match_array(["can't be blank",
+                             'Only one word containing more than two letters'])
+        end
+
+        it_behaves_like 'invalid order attributes', :first_name
       end
 
-      it 'render errors' do
-        expect(json['errors']['amount']).
-          to match_array(["can't be blank", 'is not a number'])
+      context 'with invalid last_name' do
+        it 'render errors' do
+          post :create,
+            params: { order: attributes_for(:order, last_name: nil) }
+
+          expect(json['errors']['last_name'])
+            .to match_array(["can't be blank",
+                             'Only one word containing more than two letters'])
+        end
+
+        it_behaves_like 'invalid order attributes', :last_name
+      end
+
+      context 'with invalid email' do
+        it 'render errors' do
+          post :create, params: { order: attributes_for(:order, email: nil) }
+          expect(json['errors']['email'])
+            .to match_array(["can't be blank", 'is invalid'])
+        end
+
+        it_behaves_like 'invalid order attributes', :email
       end
     end
   end
---
+++ spec/support/shared_examples/invalid_attributes_examples.rb
@@ -0,0 +1,13 @@
+RSpec.shared_examples 'invalid order attributes' do |attribute|
+  it 'render http bad request' do
+    post :create, params: { order: attributes_for(:order, attribute => nil) }
+    expect(response).to have_http_status(:bad_request)
+  end
+
+  it 'does not save the new order in database' do
+    expect do
+      post :create,
+        params: { order: attributes_for(:order, attribute => nil) }
+    end.not_to change(Order, :count)
+  end
+end
