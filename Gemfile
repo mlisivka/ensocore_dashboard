4476099e6171c26d77ce5c2599140bf91e9d5083
Mykola <mykola.lisivka@gmail.com>
2018-10-07 21:36:32 +0300

    Implemented sorting of the orders

--- Gemfile
+++ Gemfile
@@ -36,6 +36,10 @@ gem 'jbuilder', '~> 2.5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+gem 'bootstrap'
+gem 'jquery-rails'
+gem 'jquery-datatables-rails'
+
 group :development, :test do
   gem 'rspec-rails'
   gem 'factory_bot_rails'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,15 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require popper
+//= require bootstrap
+//= require dataTables/jquery.dataTables
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function () {
+  $('#ordersTable').DataTable();
+});
