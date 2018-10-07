f4432e0344053ade7f4f7ea332aca1fee90ea777
Mykola <mykola.lisivka@gmail.com>
2018-10-07 21:46:49 +0300

    Added seeds

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   validates_presence_of :email, :first_name, :last_name, :amount
-  validates :email, format: { with: /\A\w+@\w+\.\w+\z/ }
+  validates :email, format: { with: /\A[\w.]+@\w+\.\w+\z/ }
   validates :first_name, format: { with: /\A\w{2,}\z/,
                                    message: 'Only one word containing more than two letters' }
   validates :last_name, format: { with: /\A\w{2,}\z/,
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Order.delete_all
+
+Order.create([
+  { first_name: 'Daenerys', last_name: 'Targaryen',
+    email: 'd.targaryen@gmail.com', amount: rand(1..1000)},
+  { first_name: 'Jorah', last_name: 'Mormont',
+    email: 'j.mormont@gmail.com', amount: rand(1..1000)},
+  { first_name: 'John', last_name: 'Snow',
+    email: 'j.snow@gmail.com', amount: rand(1..1000)},
+])
